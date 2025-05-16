e0029141c52b1e78480a63af58ce5e638f3f6976
Bruce Hyslop <bruce.hyslop@csiro.au>
2025-05-16 11:25:06 +1000

    added extra config to build components

---
+++ packages/react-components/config.js
@@ -0,0 +1,7 @@
+const config = {
+    baseUrl: 'http://events.dev.ala.org.au',
+    authServer: 'https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_OOXU9GW39',
+    authClientID: 'change-me',
+    collectionsServer: "https://collections-test.ala.org.au",
+    biocacheUrl: "https://biocache-test.ala.org.au",
+}
