ddd0cc4c27713c5d00f6f3b4712d5f865bfbccef
yasima-csiro <yasima.kankanamge@csiro.au>
2026-02-20 10:20:05 +1100

    Fix timeout issue

--- packages/graphql-api/src/resources/shared/resources/event/event.source.js
+++ packages/graphql-api/src/resources/shared/resources/event/event.source.js
@@ -9,6 +9,8 @@ class EventAPI extends RESTDataSource {
     super();
     this.config = config;
     this.baseURL = config.apiEs;
+    // Disable GET request memoization to prevent abort signal reuse issues
+    this.memoizeGetRequests = false;
   }
 
   willSendRequest(request) {
