0908de6284374043b521dadc65e711f717ad356e
yasima-csiro <yasima.kankanamge@csiro.au>
2026-05-13 10:38:10 +1000

    Update auth in APIs

--- packages/es-api/src/resources/eventOccurrence/eventOccurrence.dataSource.js
+++ packages/es-api/src/resources/eventOccurrence/eventOccurrence.dataSource.js
@@ -22,7 +22,8 @@ const client = new Client({
   nodes: env.eventOccurrence.hosts,
   maxRetries: env.eventOccurrence.maxRetries || 3,
   requestTimeout: env.eventOccurrence.requestTimeout || 60000,
-  agent
+  agent,
+  httpAuth: `${env.eventOccurrence.username}:${env.eventOccurrence.password}`
 });
 
 async function query({ query, aggs, size = 20, from = 0, metrics, req }) {
