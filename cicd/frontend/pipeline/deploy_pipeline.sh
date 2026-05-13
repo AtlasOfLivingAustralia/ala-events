30224d16a2b4e8b8eecaf1718268ac1a4052af1d
Joe Lipson <joe.lipson@csiro.au>
2026-05-13 12:44:41 +1000

    misc cleanup

--- cicd/frontend/pipeline/deploy_pipeline.sh
+++ cicd/frontend/pipeline/deploy_pipeline.sh
@@ -37,13 +37,13 @@ done
 branch=$(git branch --show-current)
 
 # confirm which environment we're deploying to if it wasnt explicitly set
-if [ "$branch" = "main" ] && [ "$ENV" = "nonprod" ]; then
+if [[ ( "$branch" == "main" || "$branch" == "master" ) && "$ENV" == "nonprod" ]]; then
   echo "Deploy to production or staging?"
   echo "1) production"
   echo "2) staging"
-  read -p "Enter your choice (1 or 2): " choice
+  read -r -p "Enter your choice (1 or 2): " choice
 
-  case $choice in
+  case "$choice" in
     1)
       ENV="prod"
       ;;
